15278a96887f3946cb51010d101299ee2d0ac549
Divyasri-cloud-arch <divyasri5486@gmail.com>
2024-06-30 23:58:02 -0400

    client3.py

--- client3.py
+++ client3.py
@@ -36,4 +36,4 @@ if __name__ == "__main__":
             stock, bid_price, ask_price, price = getDataPoint(quote)
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
-        print("Ratio %s" % getRatio(price['XYZ'], price['PQR']))
+        print("Ratio %s" % getRatio(price['ABC'], price['PQR']))
